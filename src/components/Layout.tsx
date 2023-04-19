cfdd330b2061f1e72bc7e40054f38bf1dffd0431
kitechyt <kitechyt@gmail.com>
2023-04-19 15:45:34 +0100

    Created header and footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+import Header from './Header';
+import Footer from './Footer';
 import styles from "../styles/Layout.module.css";
 
 interface LayoutProps {
@@ -17,9 +19,11 @@ export default function Layout({ title, keywords, description, children }: Layou
                 <meta name="keywords" content={keywords} />
             </Head>
 
+            <Header />
             <div className={styles.container}>
                 {children}
             </div>
+            <Footer />
         </div>
     )
 }
