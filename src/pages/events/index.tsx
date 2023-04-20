c2f8ce2e920b49ad7447d724059bafd422d414f0
kitechyt <kitechyt@gmail.com>
2023-04-20 13:17:03 +0100

    added api paths and events

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -1,9 +1,26 @@
-import Layout from '@/components/Layout';
+import Layout from "@/components/Layout";
+import EventItem from "@/components/EventItem";
+import { API_URL } from '@/config/index';
 
-export default function EventsPage() {
+export async function getStaticProps() {
+    const res = await fetch(`${API_URL}/api/events`);
+    const events = await res.json();
+
+    return {
+        props: { events: events.slice(3) },
+        revalidate: 1
+    }
+}
+
+export default function EventsPage({ events }: any) {
     return (
         <Layout>
-            <h1>My Events</h1>
+            <h1>Events</h1>
+            {events.length === 0 && <h3>No events to show.</h3>}
+
+            {events.map((evt: any) => (
+                <EventItem key={evt.id} evt={evt} />
+            ))}
         </Layout>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,33 @@
+import Link from 'next/link';
 import Layout from "@/components/Layout";
+import EventItem from "@/components/EventItem";
+import { API_URL } from '@/config/index';
 
-export default function HomePage() {
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+    revalidate: 1
+  }
+}
+
+export default function HomePage({ events }: any) {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming events</h1>
+      {events.length === 0 && <h3>No events to show.</h3>}
+
+      {events.map((evt: any) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+
+      {events.length > 0 && (
+        <Link href="/events" legacyBehavior>
+          <a className="btn-secondary">View all events</a>
+        </Link>
+      )}
     </Layout>
   )
 }
